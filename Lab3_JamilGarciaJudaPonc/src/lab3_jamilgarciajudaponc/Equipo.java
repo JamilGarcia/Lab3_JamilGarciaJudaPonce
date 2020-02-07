061ea57d86cbaffac1def4ebba661679b919ca45
JamilGarcia <you@example.com>
2020-02-07 15:14:10 -0600

    Creacion de Medicos

--- Lab3_JamilGarciaJudaPonc/src/lab3_jamilgarciajudaponc/Equipo.java
+++ Lab3_JamilGarciaJudaPonc/src/lab3_jamilgarciajudaponc/Equipo.java
@@ -1,14 +1,75 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package lab3_jamilgarciajudaponc;
 
-/**
- *
- * @author Admin
- */
+import java.util.ArrayList;
+
 public class Equipo {
+    
+    private String nombre;
+    private int aniofun;
+    private int Ncampeonatos;
+    private ArrayList <Persona> lisper = new ArrayList();
+    private ArrayList <Jugadas> juga = new ArrayList();
+
+    public Equipo() {
+    }
+
+    public Equipo(String nombre, int aniofun, int Ncampeonatos) {
+        this.nombre = nombre;
+        this.aniofun = aniofun;
+        this.Ncampeonatos = Ncampeonatos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAniofun() {
+        return aniofun;
+    }
+
+    public void setAniofun(int aniofun) {
+        this.aniofun = aniofun;
+    }
+
+    public int getNcampeonatos() {
+        return Ncampeonatos;
+    }
+
+    public void setNcampeonatos(int Ncampeonatos) {
+        this.Ncampeonatos = Ncampeonatos;
+    }
+
+    public ArrayList<Persona> getLisper() {
+        return lisper;
+    }
+
+    public void setLisper(ArrayList<Persona> lisper) {
+        this.lisper = lisper;
+    }
+
+    public ArrayList<Jugadas> getJuga() {
+        return juga;
+    }
+
+    public void setJuga(ArrayList<Jugadas> juga) {
+        this.juga = juga;
+    }
+
+    @Override
+    public String toString() {
+        return "Equipo{" + "nombre=" + nombre + ", aniofun=" + aniofun + ", Ncampeonatos=" + Ncampeonatos + ", lisper=" + lisper + ", juga=" + juga + '}';
+    }
+    
+    
+    
+    
+    
+    
+    
     
 }
--- Lab3_JamilGarciaJudaPonc/src/lab3_jamilgarciajudaponc/Medico.java
+++ Lab3_JamilGarciaJudaPonc/src/lab3_jamilgarciajudaponc/Medico.java
@@ -1,14 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lab3_jamilgarciajudaponc;
 
-/**
- *
- * @author Admin
- */
 public class Medico {
+    
+    private String colegio;
+
+    public Medico() {
+        super();
+    }
+    
+
+    public Medico(String colegio) {
+        this.colegio = colegio;
+    }
+
+    public String getColegio() {
+        return colegio;
+    }
+
+    public void setColegio(String colegio) {
+        this.colegio = colegio;
+    }
+
+    @Override
+    public String toString() {
+        return "Medico{" + "colegio=" + colegio + '}';
+    }
+    
     
 }
